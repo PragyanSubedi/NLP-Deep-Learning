91279af07e8e6489ec2474ef9a241c4f6859c521
Pragyan Subedi <pragyansubedi@gmail.com>
2023-04-14 22:51:59 +0545

    Started working on Attention model:

--- src/imports.py
+++ src/imports.py
@@ -4,11 +4,12 @@ from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from keras.models import Sequential
-from keras.layers.recurrent import LSTM, GRU,SimpleRNN
+from keras.layers import LSTM, GRU, SimpleRNN
 from keras.layers.core import Dense, Activation, Dropout
-from keras.layers.embeddings import Embedding
+from tensorflow.keras.layers import Embedding
 from tensorflow.keras.layers import BatchNormalization
 from keras.utils import np_utils
+from keras.utils import pad_sequences
 from sklearn import preprocessing, decomposition, model_selection, metrics, pipeline
 from keras.layers import GlobalMaxPooling1D, Conv1D, MaxPooling1D, Flatten, Bidirectional, SpatialDropout1D
 from keras.preprocessing import sequence, text
@@ -21,12 +22,31 @@ from plotly import graph_objs as go
 import plotly.express as px
 import plotly.figure_factory as ff
 
-import os
-  
-# checking if the directory demo_folder 
-# exist or not.
-if not os.path.exists("data"):
-      
-    # if the demo_folder directory is not present 
-    # then create it.
-    os.makedirs("data")
+def prepare_data():
+    """
+    We will drop the other columns and approach this problem as a Binary Classification Problem and also we will have our exercise done on a smaller subsection of the dataset(only 12000 data points) to make it easier to train the models.
+    """
+    train = pd.read_csv('./data/jigsaw-toxic-comment-train.csv')
+    validation = pd.read_csv('./data/validation.csv')
+    test = pd.read_csv('./data/test.csv')
+
+    train.drop(['severe_toxic','obscene','threat','insult','identity_hate'],axis=1,inplace=True)
+
+    train = train.loc[:2000,:]
+    
+    xtrain, xvalid, ytrain, yvalid = train_test_split(train.comment_text.values, train.toxic.values, 
+                                                  stratify=train.toxic.values, 
+                                                  random_state=42, 
+                                                  test_size=0.2, shuffle=True)
+
+    return xtrain, xvalid, ytrain, yvalid
+
+def roc_auc(predictions,target):
+    '''
+    This methods returns the AUC Score when given the Predictions
+    and Labels
+    '''
+    
+    fpr, tpr, thresholds = metrics.roc_curve(target, predictions)
+    roc_auc = metrics.auc(fpr, tpr)
+    return roc_auc
